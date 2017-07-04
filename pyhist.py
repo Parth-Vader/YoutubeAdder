1a70ba0da0ea696dd0759f062eb295161eceb6ba
Parth-vader <vermaparth97@gmail.com>
2017-07-04 19:05:20 +0530

    file added

---
+++ pyhist.py
@@ -0,0 +1,34 @@
+import getpass
+import sqlite3
+import shutil
+import os
+
+def Find_path():
+	User_profile = getpass.getuser()
+	History_path = "/home/"+ User_profile + r"/.config/google-chrome/Default/History" #Usually this is where the chrome history file is located, change it if you need to.
+	new = os.getcwd() + "/temp.db"
+	#print History_path
+	return History_path, new
+
+def Main():
+	data_base, new_location = Find_path()
+	
+	shutil.copyfile(data_base,new_location)
+	conn = sqlite3.connect(data_base)
+	c = conn.cursor()
+
+	try:
+		c.execute("SELECT url FROM urls WHERE url LIKE 'https://www.youtube.com/watch?v=%' ORDER BY last_visit_time;")
+		all_rows = c.fetchall()
+		print(all_rows)
+	
+	except sqlite3.OperationalError:
+		conn = sqlite3.connect(new_location)
+		c = conn.cursor()
+		c.execute("SELECT url FROM urls WHERE url LIKE 'https://www.youtube.com/watch?v=%' ORDER BY last_visit_time;")
+		all_rows = c.fetchall()
+		print(all_rows)
+		os.remove("temp.db")
+
+if __name__ == '__main__':
+	Main()
